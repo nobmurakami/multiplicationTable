99af1fc470186e7dca3680f5dacfb969d70786c7
nobmurakami <mhpdm541@gmail.com>
2021-04-03 16:07:06 +0900

    九九表完成

--- index.php
+++ index.php
@@ -3,17 +3,38 @@
 <head>
   <meta charset="utf-8">
   <title>九九表</title>
-  <!-- <link rel="stylesheet" type="text/css" href="stylesheet.css"> -->
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
+  <h1>九九表</h1>
+  <p>偶数にのみ背景色をつけている</p>
+  <table>
+    <!-- 1行目（列の見出し） -->
+    <tr>
+      <th></th>
+      <?php for ($i = 1; $i <= 9; $i++): ?>
+        <th><?php echo $i ?></th>
+      <?php endfor ?>
+    </tr>
 
-  <?php
-    // for文・while文・foreach文のいずれかを使って、九九表を作る
-    // 偶数にのみ背景色をつける
+    <!-- 2行目以降 -->
+    <?php for ($i = 1; $i <= 9; $i++): ?>
+      <tr>
 
+        <!-- 行の見出し -->
+        <th><?php echo $i ?></th>
 
+        <?php for ($j = 1; $j <= 9; $j++): ?>
+          <!-- もし偶数だったら背景色を設定したclassを適用する -->
+          <?php if ($i * $j % 2 == 0): ?>
+            <td class="even"><?php echo $i * $j ?></td>
+          <?php else: ?>
+            <td><?php echo $i * $j ?></td>
+          <?php endif ?>
+        <?php endfor ?>
 
-  ?>
-
+      </tr>
+    <?php endfor ?>
+  </table>
 </body>
 </html>
