3d95f59d764b35ba7ae290f8bd518004f9bdaa89
nobmurakami <mhpdm541@gmail.com>
2021-04-03 15:10:53 +0900

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>九九表</title>
+  <!-- <link rel="stylesheet" type="text/css" href="stylesheet.css"> -->
+</head>
+<body>
+
+  <?php
+    // for文・while文・foreach文のいずれかを使って、九九表を作る
+    // 偶数にのみ背景色をつける
+
+
+
+  ?>
+
+</body>
+</html>
