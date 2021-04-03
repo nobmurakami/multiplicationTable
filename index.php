51255bf7215cb9576ad7ea69678070bd389145db
nobmurakami <mhpdm541@gmail.com>
2021-04-03 16:16:34 +0900

    中央寄せ

--- index.php
+++ index.php
@@ -6,35 +6,37 @@
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-  <h1>九九表</h1>
-  <p>偶数にのみ背景色をつけている</p>
-  <table>
-    <!-- 1行目（列の見出し） -->
-    <tr>
-      <th></th>
-      <?php for ($i = 1; $i <= 9; $i++): ?>
-        <th><?php echo $i ?></th>
-      <?php endfor ?>
-    </tr>
-
-    <!-- 2行目以降 -->
-    <?php for ($i = 1; $i <= 9; $i++): ?>
+  <div class="content">
+    <h1>九九表</h1>
+    <p>偶数にのみ背景色をつけている</p>
+    <table>
+      <!-- 1行目（列の見出し） -->
       <tr>
-
-        <!-- 行の見出し -->
-        <th><?php echo $i ?></th>
-
-        <?php for ($j = 1; $j <= 9; $j++): ?>
-          <!-- もし偶数だったら背景色を設定したclassを適用する -->
-          <?php if ($i * $j % 2 == 0): ?>
-            <td class="even"><?php echo $i * $j ?></td>
-          <?php else: ?>
-            <td><?php echo $i * $j ?></td>
-          <?php endif ?>
+        <th></th>
+        <?php for ($i = 1; $i <= 9; $i++): ?>
+          <th><?php echo $i ?></th>
         <?php endfor ?>
-
       </tr>
-    <?php endfor ?>
-  </table>
+
+      <!-- 2行目以降 -->
+      <?php for ($i = 1; $i <= 9; $i++): ?>
+        <tr>
+
+          <!-- 行の見出し -->
+          <th><?php echo $i ?></th>
+
+          <?php for ($j = 1; $j <= 9; $j++): ?>
+            <!-- もし偶数だったら背景色を設定したclassを適用する -->
+            <?php if ($i * $j % 2 == 0): ?>
+              <td class="even"><?php echo $i * $j ?></td>
+            <?php else: ?>
+              <td><?php echo $i * $j ?></td>
+            <?php endif ?>
+          <?php endfor ?>
+
+        </tr>
+      <?php endfor ?>
+    </table>
+  </div>
 </body>
 </html>
